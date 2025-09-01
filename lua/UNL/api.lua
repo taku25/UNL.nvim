d70066b25fa9794d55f98479865dd144bb331134
taku25 <taku36@gmail.com>
2025-09-01 15:58:57 +0900

    add new api

--- lua/UNL/api.lua
+++ lua/UNL/api.lua
@@ -1,4 +1,6 @@
+local unl_finder = require("UNL.finder")
 local M = {}
+
 
 function M.get_progress_component()
   local ok, progress_status = pcall(require, "UNL.backend.progress.status")
@@ -14,6 +16,20 @@ function M.get_progress_component()
 
   -- パーセント記号は statusline のために %% とエスケープする必要がある
   return string.format("%s %s %3d%%%% %s", status.title, spinner, status.percentage, status.message)
+end
+
+
+
+function M.find_project(file_path, opts)
+  return unl_finder.project.find_project(file_path, opts)
+end
+
+function M.find_module(file_path, opts)
+  return unl_finder.module.find_module_root(file_path, opts)
+end
+
+function M.find_engine(project_file_path, opts)
+  return unl_finder.engine.find_engine_root(project_file_path, opts)
 end
 
 return M
